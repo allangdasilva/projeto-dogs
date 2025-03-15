cbf58bbc007b242efde553288511605242af0a54
Allan Silva <allandasilva33@outlook.com>
2025-03-15 11:23:25 -0300

    Estrutura base criada | header, home, footer e login

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,31 @@
 import React from "react";
 import "./App.css";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Footer from "./components/Footer";
+import Login from "./components/login/Login";
+import LoginForm from "./components/login/LoginForm";
+import LoginCreate from "./components/login/LoginCreate";
+import LoginPasswordLost from "./components/login/LoginPasswordLost";
+import LoginPasswordReset from "./components/login/LoginPasswordReset";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 const App = () => {
   return (
     <>
-      <h1>App React</h1>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="login" element={<Login />}>
+            <Route path="" element={<LoginForm />} />
+            <Route path="create" element={<LoginCreate />} />
+            <Route path="lost" element={<LoginPasswordLost />} />
+            <Route path="reset" element={<LoginPasswordReset />} />
+          </Route>
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </>
   );
 };
---
+++ src/components/login/Login.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from "./Login.module.css";
+import { Link, Outlet } from "react-router-dom";
+
+const Login = () => {
+  return (
+    <>
+      <Outlet />
+    </>
+  );
+};
+
+export default Login;
