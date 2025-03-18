6409930fba04cfaff4dc298561ed2dfdca52e10d
Allan Silva <allandasilva33@outlook.com>
2025-03-18 19:32:01 -0300

    Primeira parte do modal do feed completa

--- src/components/feed/FeedPhotos.jsx
+++ src/components/feed/FeedPhotos.jsx
@@ -3,17 +3,16 @@ import FeedPhotosItem from "./FeedPhotosItem";
 import useFetch from "../../hooks/useFetch";
 import Error from "../helper/Error";
 import Loading from "../helper/Loading";
-import { PHOTO_GET } from "../../api";
+import { PHOTOS_GET } from "../../api";
 import styles from "./FeedPhotos.module.css";
 
-const FeedPhotos = () => {
+const FeedPhotos = ({ setModalPhoto }) => {
   const { data, error, loading, request } = useFetch();
 
   React.useEffect(() => {
     async function fetchPhotos() {
-      const { url, options } = PHOTO_GET({ page: 1, total: 6, user: 0 });
+      const { url, options } = PHOTOS_GET({ page: 1, total: 6, user: 0 });
       const { response, json } = await request(url, options);
-      console.log(json);
     }
     fetchPhotos();
   }, [request]);
@@ -25,7 +24,11 @@ const FeedPhotos = () => {
       <>
         <ul className={`${styles.feed} animeLeft`}>
           {data.map((photo) => (
-            <FeedPhotosItem key={photo.id} photo={photo} />
+            <FeedPhotosItem
+              key={photo.id}
+              photo={photo}
+              setModalPhoto={setModalPhoto}
+            />
           ))}
         </ul>
       </>
