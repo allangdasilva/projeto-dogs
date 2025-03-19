eb20d0ac7553ebeff9469f1c599e7367bc9ca7ed
Allan Silva <allandasilva33@outlook.com>
2025-03-19 02:36:31 -0300

    Scroll infinito

--- src/components/user/Account.jsx
+++ src/components/user/Account.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import UserHeader from "./UserHeader";
 import { Outlet } from "react-router-dom";
+import { UserContext } from "../../UserContext";
 
 const Account = () => {
+  const { data } = React.useContext(UserContext);
+  const user = data.id;
   return (
     <>
       <section className="container">
         <UserHeader />
-        <Outlet />
+        <Outlet context={{ user }} />
       </section>
     </>
   );
