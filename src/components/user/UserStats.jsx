6a0519c9100df67c27abaf6598e7d871f757cf59
Allan Silva <allandasilva33@outlook.com>
2025-03-21 00:42:48 -0300

    Primeira parte da página de estatísticas do usuário

--- src/components/user/UserStats.jsx
+++ src/components/user/UserStats.jsx
@@ -1,13 +1,32 @@
 import React from "react";
 import Head from "../helper/Head";
+import useFetch from "../../hooks/useFetch";
+import { STATS_GET } from "../../api";
+import Loading from "../helper/Loading";
+import Error from "../helper/Error";
+import UserStatsGraph from "./UserStatsGraph";
 
 const UserStats = () => {
-  return (
-    <div>
-      <Head title={"Estatísticas"} description={"Estatísticas"} />
-      UserStats
-    </div>
-  );
+  const { data, loading, error, request } = useFetch();
+
+  React.useEffect(() => {
+    async function getStats() {
+      const { url, options } = STATS_GET();
+      await request(url, options);
+    }
+    getStats();
+  }, [request]);
+
+  if (loading) return <Loading />;
+  if (error) return <Error error={error} />;
+  if (data)
+    return (
+      <div>
+        <Head title={"Estatísticas"} description={"Estatísticas"} />
+        <UserStatsGraph data={data} />
+      </div>
+    );
+  else return null;
 };
 
 export default UserStats;
